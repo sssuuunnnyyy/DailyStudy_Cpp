c8c48698552efdfc6fe6a549500ea32c412e3d07
Sunny <leesonia09@gmail.com>
2022-06-21 09:02:46 +0900

    f

--- June_Week4/quiz_0620.cpp
+++ June_Week4/quiz_0620.cpp
@@ -1,54 +1,53 @@
-//#include<iostream>
-//
-//using namespace std;
-//
-//float GetStd_weight(float height)
-//{
-//	float std_weight = (height - 100) * 0.9;
-//	return std_weight;
-//}
-//
-//float GetBmi(float weight, float GetStd_weight)
-//{
-//	return (weight - GetStd_weight) * 100 / GetStd_weight;
-//}
-//
-//void FatLevel(int GetBmi)
-//{
-//
-//	if (GetBmi <= 10)
-//	{
-//		cout << "Normal" << endl;
-//	}
-//	else if (GetBmi > 10 && GetBmi <= 20)
-//	{
-//		cout << "Overweight" << endl;
-//	}
-//	else (GetBmi > 20)
-//	{
-//		cout << "Obesity" << endl;
-//	}
-//
-//}
-//
-//
-//int main()
-//{
-//	float GetStd_weight(float height);
-//	float GetBmi(float weight, float GetStd_weight);
-//	void FatLevel(int GetBmi);
-//	
-//
-//	//#1. 비만도 구하기
-//	float weight, height = { 0 };
-//	cout << "Height: ";
-//	cin >> height;
-//	cout << "Weight: ";
-//	cin >> weight;
-//
-//	cout << "BMI: " << GetBmi<< FatLevel << endl;
+#include<iostream>
 
-//}
+using namespace std;
+
+float GetStd_weight(float height)
+{
+	float std_weight = (height - 100) * 0.9;
+	return std_weight;
+}
+
+float GetBmi(float weight, float GetStd_weight)
+{
+	return ((weight - GetStd_weight) * 100) / GetStd_weight;
+}
+
+void FatLevel(float GetBmi)
+{
+
+	if (GetBmi <= 10)
+	{
+		cout << "Normal" << endl;
+	}
+	else if (GetBmi > 10 && GetBmi <= 20)
+	{
+		cout << "Overweight" << endl;
+	}
+	else if (GetBmi > 20)
+	{
+		cout << "Obesity" << endl;
+	}
+
+}
+
+
+int main()
+{
+	
+	
+
+	//#1. 비만도 구하기
+	float weight, height = { 0 };
+	cout << "Height: ";
+	cin >> height;
+	cout << "Weight: ";
+	cin >> weight;
+	//GetBmi(weight, GetStd_weight(height));
+	FatLevel(GetBmi(weight, GetStd_weight(height)));
+
+
+}
 
 //#2. Programmers
     // x만큼 간격이 있는 n개의 숫자
@@ -66,4 +65,4 @@
 //
 //    }
 //    return v;
-}
+//}
